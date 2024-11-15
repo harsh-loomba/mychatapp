190a70329debbaf0e7568721b60acc2613994441
harsh-loomba <95173549+harsh-loomba@users.noreply.github.com>
2024-11-15 15:36:33 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -8,14 +8,6 @@ import { getStorage } from "firebase/storage";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-	apiKey: "AIzaSyAjsLlXIwPaDbvszWu7GNxd1y3UC8p0S9I",
-	authDomain: "chat-ba572.firebaseapp.com",
-	projectId: "chat-ba572",
-	storageBucket: "chat-ba572.appspot.com",
-	messagingSenderId: "192648033976",
-	appId: "1:192648033976:web:af0dcc831dcb4c80d5a64d",
-};
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
